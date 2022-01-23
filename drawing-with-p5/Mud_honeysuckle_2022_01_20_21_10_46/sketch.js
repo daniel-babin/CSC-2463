8f7e468bcbaff4ca47cf4d35d8bbc3e06ad454c5
daniel-babin <97774747+daniel-babin@users.noreply.github.com>
2022-01-23 17:22:14 -0600

    example-1

---
+++ drawing-with-p5/Mud_honeysuckle_2022_01_20_21_10_46/sketch.js
@@ -0,0 +1,10 @@
+//Remeber to save CTRL+S to update Chrome tab
+function setup() {
+  createCanvas(800, 400);
+}
+
+function draw() {
+  background(10, 250, 40);
+  circle(200,200,350)
+  rect(450,40,325,320)
+}
