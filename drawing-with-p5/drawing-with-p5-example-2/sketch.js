c7aebf73369cb938ab0ccb48b75d6f53b1bf6a86
daniel-babin <97774747+daniel-babin@users.noreply.github.com>
2022-01-23 19:40:32 -0600

    Add example-2

---
+++ drawing-with-p5/drawing-with-p5-example-2/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(250);
+  noStroke();
+  circle(250, 150, 150,);
+    fill(0, 0, 250, 100);
+  circle(190, 250, 150);
+    fill(0, 250, 0, 100);
+  circle(310, 250, 150);
+    fill(250, 0, 0, 100);
+}
