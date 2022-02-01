0f1b8ed9fd850e0f11207d0f3fb99b59216ccbdc
daniel-babin <dbabi23@lsu.edu>
2022-02-01 14:45:56 -0600

    Load Turtle

--- SpriteAnimation/sketch.js
+++ SpriteAnimation/sketch.js
@@ -1,7 +1,15 @@
-function setup() {
-  createCanvas(400, 400);
+let img;
+
+function preload(){
+  img = loadImage("turtle.png");
 }
 
-function draw() {
-  background(220);
+function setup(){
+  createCanvas(600, 600);
+}
+
+function draw(){
+  background(200);
+  image(img, 10, 10, 100, 100);
+
 }
