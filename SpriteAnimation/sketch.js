45445fc34beb741f4f98b95f4ff36ca378124d00
daniel-babin <dbabi23@lsu.edu>
2022-02-03 14:53:06 -0600

    Starting Viking

--- SpriteAnimation/sketch.js
+++ SpriteAnimation/sketch.js
@@ -1,15 +1,51 @@
-let img;
+let character;
 
-function preload(){
-  img = loadImage("turtle.png");
+function preload() {
+  spriteSheet = loadImage("Viking.png");
 }
 
-function setup(){
+function setup() {
   createCanvas(600, 600);
+  imageMode(CENTER);
+
+  character = new CharacterData(spriteSheet, 0, 300);
 }
 
-function draw(){
-  background(200);
-  image(img, 10, 10, 100, 100);
+function keyPressed() {
+  if(keyCode == RIGHT_ARROW) {
+    character.go(1);
+  }
+  else if(keyCode == LEFT_ARROW) {
+    character.go(-1);
+  }
+}
 
+function keyReleased() {
+  character.stop();
+}
+
+function draw() {
+  background(255);
+  character.draw();
+}
+
+class Character {
+  constructor(spriteSheet, x, y) {
+    this.spriteSheet = spriteSheet;
+    this.sx = 0;
+    this.x = x;
+    this.y = y;
+    this.move = 0;
+    this.facing = 1;
+  }
+
+  draw() {
+    push();
+    translate(this.x, this.y);
+    scale(this.facing, 1);
+      
+    if(this.move == 0) {
+
+    }
+  }
 }
