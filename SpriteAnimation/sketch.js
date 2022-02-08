bd5951953820aabf0b62ac79a5e736adc6969d01
daniel-babin <dbabi23@lsu.edu>
2022-02-08 13:31:42 -0600

    Finish Assignment

--- SpriteAnimation/sketch.js
+++ SpriteAnimation/sketch.js
@@ -1,32 +1,45 @@
 let character;
+let character2;
 
 function preload() {
   spriteSheet = loadImage("Viking.png");
+  spriteSheet2 = loadImage("Lime.png");
+  spriteSheet3 = loadImage("Yellow.png");
 }
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(1920, 969);
   imageMode(CENTER);
 
-  character = new CharacterData(spriteSheet, 0, 300);
+  character = new Character(spriteSheet, random(100, 1900), random(0, 950));
+  character2 = new Character(spriteSheet2, random(100, 1900), random(0, 950));
+  character3 = new Character(spriteSheet3, random(100, 1900), random(0, 950));
 }
 
 function keyPressed() {
   if(keyCode == RIGHT_ARROW) {
     character.go(1);
+    character2.go(1);
+    character3.go(1);
   }
   else if(keyCode == LEFT_ARROW) {
     character.go(-1);
+    character2.go(-1);
+    character3.go(-1);
   }
 }
 
 function keyReleased() {
   character.stop();
+  character2.stop();
+  character3.stop();
 }
 
 function draw() {
   background(255);
   character.draw();
+  character2.draw();
+  character3.draw();
 }
 
 class Character {
@@ -45,7 +58,26 @@ class Character {
     scale(this.facing, 1);
       
     if(this.move == 0) {
-
+      image(this.spriteSheet, 0, 0, 200, 200, 0, 0, 80, 80);
     }
+    else{
+      image(this.spriteSheet, 0, 0, 200, 200, 80 * (this.sx +1), 0, 80, 80);
+    }
+
+    if(frameCount % 5 == 0){
+      this.sx = (this.sx + 1) % 8;
+    }
+    this.x += 2 * this.move;
+    pop();
+  }
+
+  go(direction){
+    this.move = direction;
+    this.facing = direction;
+    this.sx = 3;
+  }
+
+  stop() {
+    this.move = 0;
   }
 }
