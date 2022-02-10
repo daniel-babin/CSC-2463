be7dab5a02e0babfb394a2605234d9ce3a66d8f3
daniel-babin <dbabi23@lsu.edu>
2022-02-10 16:15:50 -0600

    Update sketch.js

--- BigSquish/sketch.js
+++ BigSquish/sketch.js
@@ -1,3 +1,16 @@
+//Universal variables
+let bgImage;
+let bugImage = [];
+let imageURL = "https://github.com/daniel-babin/CSC-2463/tree/main/BigSquish/BigSquichImages/"
+let walls = [];
+let directions = [0, 90, 180, 270];
+let bugSpeed = 1.5;
+
+//Preload images for game
+function preload(){
+  bgImage = loadImage(imageURL + "background.png");
+}
+
 //Create game canvas
 function setup() {
   createCanvas(800, 800);
@@ -5,5 +18,5 @@ function setup() {
 
 //Physical game mechanics
 function draw() {
-  background(220);
+  background(bgImage);
 }
