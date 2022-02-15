5a58f2843e1ba9bb1f21ff891e067773a0de17bc
daniel-babin <dbabi23@lsu.edu>
2022-02-15 14:43:12 -0600

    update

--- BuildASampler/sketch.js
+++ BuildASampler/sketch.js
@@ -1,10 +1,21 @@
 //In class Feb 15
-let sound1 = new tone.Player
+let sound1 = new tone.Player('media/Extra%20Life%20Sound%20Effect%20(%20FREE%20DOWNLOAD%20).wav');
 
 function setup() {
   createCanvas(400, 400);
+  sound1.toDestination();
 }
 
 function draw() {
   background(220);
+}
+
+function keyPressed() {
+  if(key === "1") {
+    sound1.playbackRate = 1;
+    sound1.start();
+  }
+  
+  sound1.playbackRate = (mouseY / 200) + 0.01;
+  sound1.start
 }
