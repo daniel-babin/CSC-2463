e2cf853df04e9ab74fdd40aba08e6e6a095ad15e
daniel-babin <dbabi23@lsu.edu>
2022-02-21 23:39:14 -0600

    done

--- BuildASampler/js/sketch.js
+++ BuildASampler/js/sketch.js
@@ -15,28 +15,28 @@ let button1, button2, button3, button4, button5;
 let slider1;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
   sounds.connect(delay);
   delay.toDestination();
 
   button1 = createButton("1: Among Us");
-  button1.position(50, 175);
+  button1.position(100, 200);
   button1.mousePressed( () => buttonSound('amongUs') );
   
   button2 = createButton("2: bonk");
-  button2.position(150, 175);
+  button2.position(240, 200);
   button2.mousePressed( () => buttonSound('bonk') );
 
   button3 = createButton('3: hehe');
-  button3.position(225, 175);
+  button3.position(350, 200);
   button3.mousePressed( () => buttonSound('hehe') );
 
   button4 = createButton('4: vine');
-  button4.position(120, 200);
+  button4.position(200, 300);
   button4.mousePressed( () => buttonSound('vine') );
 
   button5 = createButton('5: dog');
-  button5.position(200, 200);
+  button5.position(300, 300);
   button5.mousePressed( () => buttonSound('dog') );
   
   slider1 = createSlider(0, 1, 0.5, 0.1);
@@ -49,10 +49,10 @@ function setup() {
 function draw() {
   background(220);
   textSize(25);
-  text('Press buttons or click number keys', 5, 150);
+  text('Press buttons or click number keys', 105, 150);
 
   textSize(15);
-  text('Slide to change delay', 5, 390);
+  text('Slide to change delay', 5, 590);
 }
 
 // Number keys to play sounds
