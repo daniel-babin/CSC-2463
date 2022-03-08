b42a01560d22030973ad7d814210aa9826766fa1
daniel-babin <dbabi23@lsu.edu>
2022-03-07 22:43:58 -0600

    add sound
    not playing on Chrome.. uncaught reference error

--- 2.3 Sound Synthesis & Sound Effects/js/sketch.js
+++ 2.3 Sound Synthesis & Sound Effects/js/sketch.js
@@ -5,55 +5,134 @@ function preload() {
   img = loadImage("media/jetsons.png");
 }
 
-// Set up Tone
-let osc = new Tone.AMOscillator(pitch,'sine','sine').start()
-let gain = new Tone.Gain().toDestination();
-let pan = new Tone.Panner().connect(gain);
-let ampEnv = new Tone.AmplitudeEnvelope({
-  attack: 0.1,
-  decay: 0.2,
-  sustain: 1.0,
-  release: 0.8
-}).connect(pan);
-osc.connect(ampEnv);
-
-let freqLFO = new Tone.LFO(4,300,1000).start();
-freqLFO.connect(osc.frequency); 
-
-
-let noise = new Tone.Noise('pink').start();
-let noiseEnv = new Tone.AmplitudeEnvelope({
-  attack: 0.1,
-  decay: 0.2,
-  sustain: 1.0,
-  release: 0.8
-}).connect(gain);
-let noiseFilter = new Tone.Filter(800,'lowpass').connect(noiseEnv);
-noise.connect(noiseFilter);
-
+function make_Instrument() {
+  // create synth
+  var instrument = new Tone.DuoSynth();
+  var synthJSON = {
+      "vibratoAmount"  : 0.5 ,
+    "vibratoRate"  : 5 ,
+    "harmonicity"  : 1.5 ,
+    "voice0"  : {
+      "volume"  : -20 ,
+      "portamento"  : 0 ,
+      "oscillator"  : {
+          "type"  : "sine"
+      }  ,
+      "filterEnvelope"  : {
+        "attack"  : 0.01 ,
+        "decay"  : 0 ,
+        "sustain"  : 1 ,
+        "release"  : 0.5
+      }  ,
+      "envelope"  : {
+        "attack"  : 0.01 ,
+        "decay"  : 0 ,
+        "sustain"  : 1 ,
+        "release"  : 0.5
+      }
+    }  ,
+    "voice1"  : {
+      "volume"  : -20 ,
+      "portamento"  : 0 ,
+      "oscillator"  : {
+          "type"  : "sine"
+      }  ,
+      "filterEnvelope"  : {
+        "attack"  : 0.01 ,
+        "decay"  : 0 ,
+        "sustain"  : 1 ,
+        "release"  : 0.5
+      }  ,
+      "envelope"  : {
+        "attack"  : 0.01 ,
+        "decay"  : 0 ,
+        "sustain"  : 1 ,
+        "release"  : 0.5
+      }
+      }
+  };
+  
+  instrument.set(synthJSON);
+  
+  var effect1, effect2, effect3;
+  
+  // create effects
+  var effect1 = new Tone.Phaser();
+  effect1JSON = {
+    "frequency": 12,
+    "octaves": 3.3,
+    "Q": 8,
+    "baseFrequency": 250,
+      "wet": 0.5
+  };
+  effect1.set(effect1JSON);
+  
+  var effect2 = new Tone.Distortion();
+  effect2JSON = {
+    "distortion" : 0.08, 
+    "wet" : 0.3
+  };
+  effect2.set(effect2JSON);
+  
+  var effect3 = new Tone.Chorus();
+  effect3JSON = {
+    "frequency": 0.2,
+    "delayTime": 20,
+    "type": "sine",
+    "depth": 1,
+    "feedback": 0.45,
+    "spread": 180,
+      "wet": 0.5
+  };
+  effect3.set(effect3JSON);
+  
+  // make connections
+  instrument.connect(effect1);
+  effect1.connect(effect2);
+  effect2.connect(effect3);
+  effect3.connect(Tone.Master);
+  
+  // define deep dispose function
+  function deep_dispose() {
+      if(effect1 != undefined && effect1 != null) {
+          effect1.dispose();
+          effect1 = null;
+      }
+      if(effect2 != undefined && effect2 != null) {
+          effect2.dispose();
+          effect2 = null;
+      }
+      if(effect3 != undefined && effect3 != null) {
+          effect3.dispose();
+          effect3 = null;
+      }
+      if(instrument != undefined && instrument != null) {
+          instrument.dispose();
+          instrument = null;
+      }
+  }
+  
+  
+  return {
+      instrument: instrument,
+      deep_dispose: deep_dispose
+      };
+  
+  }
 
 function setup() {
   createCanvas(400, 400);
-  imageMode(CENTER);
-  image(img, 50, 50, 100, 100);
 }
 
 function draw() {
-  background(249, 142, 29);
   textSize(30);
-  text("Press to Start", 100, 100);
+  text("Press to Start", 130, 150);
 }
 
 function mousePressed() {
   Tone.start();
   console.log('pressed');
-  ampEnv.triggerAttackRelease('4n');
-  // osc.frequency.linearRampTo(400,'+1');
-  osc.frequency.linearRampToValueAtTime(pitch-200,'+1');
-  ampEnv.triggerAttackRelease('4n','+1');
-
-  if(mouseY > 400) {
-    noiseFilter.frequency.value = mouseX +100;
-    noiseEnv.triggerAttackRelease(0.5);
-  }
+  image(img, 100, 170, 250, 250);
+  
+  instrument.triggerAttackRelease(5);
 }
