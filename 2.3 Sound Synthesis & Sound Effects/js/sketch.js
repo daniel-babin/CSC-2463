3ca20af371e7a32ba7e0555f063e875394756390
daniel-babin <dbabi23@lsu.edu>
2022-03-08 22:24:14 -0600

    finish

--- 2.3 Sound Synthesis & Sound Effects/js/sketch.js
+++ 2.3 Sound Synthesis & Sound Effects/js/sketch.js
@@ -1,7 +1,5 @@
-let pitch = 800;
 let img;
 var instrument;
-var synthJSON;
 var effect1, effect2, effect3;
 
 function preload() {
@@ -9,83 +7,73 @@ function preload() {
 }
 
 function make_Instrument() {
-  // create synth
-  instrument = new Tone.DuoSynth();
-  synthJSON = {
-      "vibratoAmount"  : 0.5 ,
-    "vibratoRate"  : 5 ,
-    "harmonicity"  : 1.5 ,
-    "voice0"  : {
-      "volume"  : -20 ,
-      "portamento"  : 0 ,
-      "oscillator"  : {
-          "type"  : "sine"
-      }  ,
-      "filterEnvelope"  : {
-        "attack"  : 0.01 ,
-        "decay"  : 0 ,
-        "sustain"  : 1 ,
-        "release"  : 0.5
-      }  ,
-      "envelope"  : {
-        "attack"  : 0.01 ,
-        "decay"  : 0 ,
-        "sustain"  : 1 ,
-        "release"  : 0.5
-      }
-    }  ,
-    "voice1"  : {
-      "volume"  : -20 ,
-      "portamento"  : 0 ,
-      "oscillator"  : {
-          "type"  : "sine"
-      }  ,
-      "filterEnvelope"  : {
-        "attack"  : 0.01 ,
-        "decay"  : 0 ,
-        "sustain"  : 1 ,
-        "release"  : 0.5
-      }  ,
-      "envelope"  : {
-        "attack"  : 0.01 ,
-        "decay"  : 0 ,
-        "sustain"  : 1 ,
-        "release"  : 0.5
-      }
-      }
-  };
-  
-  instrument.set(synthJSON);
+  instrument = new Tone.DuoSynth({
+    "vibratoAmount"  : 0.5 ,
+	"vibratoRate"  : 5 ,
+	"harmonicity"  : 1.5 ,
+	"voice0"  : {
+		"volume"  : -10 ,
+		"portamento"  : 0 ,
+		"oscillator"  : {
+		    "type"  : "sine"
+		}  ,
+		"filterEnvelope"  : {
+			"attack"  : 0.01 ,
+			"decay"  : 0 ,
+			"sustain"  : 1 ,
+			"release"  : 0.5
+		}  ,
+		"envelope"  : {
+			"attack"  : 0.01 ,
+			"decay"  : 0 ,
+			"sustain"  : 1 ,
+			"release"  : 0.5
+		}
+	}  ,
+	"voice1"  : {
+		"volume"  : -20 ,
+		"portamento"  : 0 ,
+		"oscillator"  : {
+		    "type"  : "sine"
+		}  ,
+		"filterEnvelope"  : {
+			"attack"  : 0.01 ,
+			"decay"  : 0 ,
+			"sustain"  : 1 ,
+			"release"  : 0.5
+		}  ,
+		"envelope"  : {
+			"attack"  : 0.01 ,
+			"decay"  : 0 ,
+			"sustain"  : 1 ,
+			"release"  : 0.5
+		}
+    }
+});
   
   // create effects
-  effect1 = new Tone.Phaser();
-  effect1JSON = {
+  effect1 = new Tone.Phaser({
     "frequency": 12,
-    "octaves": 3.3,
-    "Q": 8,
-    "baseFrequency": 250,
-      "wet": 0.5
-  };
-  effect1.set(effect1JSON);
+	"octaves": 3.3,
+	"Q": 8,
+	"baseFrequency": 250,
+    "wet": 0.5
+  });
   
-  effect2 = new Tone.Distortion();
-  effect2JSON = {
-    "distortion" : 0.08, 
-    "wet" : 0.3
-  };
-  effect2.set(effect2JSON);
-  
-  effect3 = new Tone.Chorus();
-  effect3JSON = {
+  effect2 = new Tone.Distortion({
     "frequency": 0.2,
-    "delayTime": 20,
-    "type": "sine",
-    "depth": 1,
-    "feedback": 0.45,
-    "spread": 180,
-      "wet": 0.5
-  };
-  effect3.set(effect3JSON);
+	"delayTime": 20,
+	"type": "sine",
+	"depth": 1,
+	"feedback": 0.45,
+	"spread": 180,
+    "wet": 0.5
+  });
+  
+  effect3 = new Tone.Chorus({
+    "distortion" : 0.08, 
+	"wet" : 0.3
+    });
   
   // make connections
   instrument.connect(effect1);
@@ -131,9 +119,9 @@ function draw() {
 }
 
 function mousePressed() {
-  Tone.start();
+  //Tone.start();
   console.log('pressed');
   image(img, 100, 170, 250, 250);
-  
-  instrument.triggerAttackRelease(5);
+  make_Instrument()
+  instrument.triggerAttackRelease("C7", 2.5);
 }
