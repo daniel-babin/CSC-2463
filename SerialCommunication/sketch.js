0fb63851fae815b81e05db905b44691bf7bd3275
daniel-babin <dbabi23@lsu.edu>
2022-04-10 20:39:10 -0500

    finish 3.3

--- SerialCommunication/sketch.js
+++ SerialCommunication/sketch.js
@@ -3,6 +3,9 @@
     Daniel Babin
     CSC 2463
     Dr. Allison
+
+    Draws the webpage, the shade of green of the background changes with the input from the potentiometer.
+    Also transmits the mouseX coordinate as input to turn the LED on/off.
   YouTube link:
 */
 
@@ -20,6 +23,12 @@ function setup() {
 
 function draw() {
   background(0, sensor.a0, 0)
+  stroke(255);
+  line(402, 0, 400, 600);
+
+  textSize(50);
+  text('Turn LED OFF', 40, 300);
+  text('Turn LED ON', 440, 300);
 
   //console.log(sensor.a0);
 }
